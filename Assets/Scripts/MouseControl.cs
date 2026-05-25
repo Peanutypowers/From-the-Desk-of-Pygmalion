ddc2e04fbbde0f7466751da7b42ad45860328745
ckfrancois <francois.chelseak@gmail.com>
2026-05-25 00:05:00 -0400

    Added bounds to camera

--- Assets/Scripts/MouseControl.cs
+++ Assets/Scripts/MouseControl.cs
@@ -4,6 +4,10 @@ public class CameraController : MonoBehaviour
 {
     private Vector3 mousePosition;
 	public float moveSpeed = 0.1f;
+    public float leftBound = 14f;
+    public float rightBound = 20f;
+    public float lowerBound = 10f;
+    public float upperBound = 20f;
 
     void Start()
     {
@@ -13,7 +17,22 @@ public class CameraController : MonoBehaviour
     void Update()
     {
 		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = transform.position.z;
+
+        // Bind mouse position
+        if(mousePosition.x < leftBound) {
+            mousePosition.x = leftBound;
+        }
+        if(mousePosition.x > rightBound) {
+            mousePosition.x = rightBound;
+        }
+        if(mousePosition.y < lowerBound) {
+            mousePosition.y = lowerBound;
+        }
+        if(mousePosition.y > upperBound) {
+            mousePosition.y = upperBound;
+        }
+
+        mousePosition.z = transform.position.z; // will be 0
 		transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
         //transform.position = Vector3.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
         
