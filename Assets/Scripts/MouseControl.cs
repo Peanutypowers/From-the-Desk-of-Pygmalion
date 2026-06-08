da2d776b119c9b3b2eea90821e3a14bf977a576a
ckfrancois <francois.chelseak@gmail.com>
2026-06-08 18:57:14 -0400

    Added Camera Rotation using Arrows
    - Left and Right Arrows control which way player faces

--- Assets/Scripts/MouseControl.cs
+++ Assets/Scripts/MouseControl.cs
@@ -1,21 +1,85 @@
 using UnityEngine;
+using UnityEngine.UI;
+using Unity.Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
     private Vector3 mousePosition;
-	public float moveSpeed = 0.1f;
-    public float leftBound = 14f;
-    public float rightBound = 20f;
-    public float lowerBound = 10f;
-    public float upperBound = 20f;
+	public float moveSpeed = 0.001f;
+    [SerializeField] private Button btnRight = null;
+    [SerializeField] private Button btnLeft = null;
+    [SerializeField] private CinemachineCamera vcam;
+    private CinemachineFollow vcamFollow;
+    private float currentWall = 1; // 1 is forward wall, 0 is left wall, 2 is right wall, 3 is back wall
+    private float leftBound;
+    private float rightBound;
+    private float lowerBound;
+    private float upperBound;
+
+    private void Awake() {
+        if (btnRight == null || btnLeft == null) {
+            Debug.LogError("Need to set button references.");
+        } else {
+            vcamFollow = vcam.GetComponent<CinemachineFollow>();
+            btnRight.onClick.AddListener(OnButtonRightClick);
+            btnLeft.onClick.AddListener(OnButtonLeftClick);
+        }
+    }
+
+    private void OnButtonRightClick() {
+        Debug.Log("Turning right.");
+
+        if(currentWall == 1) { // forward wall, bring to right wall
+            vcamFollow.FollowOffset = new Vector3(-10f, 0f, -10f);
+            currentWall = 2;
+        } else if (currentWall == 0) { // left wall, bring to forward wall
+            vcamFollow.FollowOffset = new Vector3(0f, 0f, -10f);
+            currentWall = 1;
+        } else if(currentWall == 2) { // right wall, bring to back wall
+            vcamFollow.FollowOffset = new Vector3(0f, 0f, 10f);
+            currentWall = 3;
+        } else if(currentWall == 3) { // back wall, bring to left wall
+            vcamFollow.FollowOffset = new Vector3(10f, 0f, -10f);
+            currentWall = 0;
+        }
+        // new Vector3(-10f, 0f, -10f) moves it to the right wall with no corner
+        // new Vector3(-10f, 0f, 10f) moves it to the right wall with corner (basically opposite of current view)
+    }
+
+    private void OnButtonLeftClick() {
+        Debug.Log("Turning left.");
+
+        if(currentWall == 1) { // forward wall, bring to left wall
+            vcamFollow.FollowOffset = new Vector3(10f, 0f, -10f);
+            currentWall = 0;
+        } else if (currentWall == 0) { // left wall, bring to back wall
+            vcamFollow.FollowOffset = new Vector3(0f, 0f, 10f);
+            currentWall = 3;
+        } else if(currentWall == 2) { // right wall, bring to forward wall
+            vcamFollow.FollowOffset = new Vector3(0f, 0f, -10f);
+            currentWall = 1;
+        } else if(currentWall == 3) { // back wall, bring to right wall
+            vcamFollow.FollowOffset = new Vector3(-10f, 0f, -10f);
+            currentWall = 2;
+        }
+    }
 
     void Start()
     {
+        calculateBounds(transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
+        /* // Changes moveSpeed to 0 gradually when the mouse isn't moving
+           // Brings player back to center of screen during this time
+        if (Input.GetAxis("Mouse X") == 0) {
+            moveSpeed = Mathf.Lerp(moveSpeed, 0f, Time.deltaTime * 5f);
+        } else {
+            moveSpeed = 0.001f;
+        }*/
+
 		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         // Bind mouse position
@@ -36,5 +100,13 @@ public class CameraController : MonoBehaviour
 		transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
         //transform.position = Vector3.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
         
+    }
+
+    // Calculate bounds based on mouseSphere's starting position
+    void calculateBounds(Vector3 targetPosition) {
+        leftBound = targetPosition.x - 5f;
+        rightBound = targetPosition.x + 5f;
+        lowerBound = targetPosition.y - 5f;
+        upperBound = targetPosition.y + 5f;
     }
 }
