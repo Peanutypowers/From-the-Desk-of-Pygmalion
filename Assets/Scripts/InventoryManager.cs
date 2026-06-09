60088337741f146e624ae13bb9943ec8301d265f
Peanutypowers <peanutypowers@gmail.com>
2026-06-09 16:16:22 -0400

    Item collection save

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -28,4 +28,9 @@ public class InventoryManager : MonoBehaviour
             menuActivated = true;
         }
     }
+
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+    {
+        Debug.Log("itemName = " + itemName + " quantity = " + quantity + " itemSprite = " + itemSprite);
+    }
 }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private string itemName;
+
+    [SerializeField]
+    private int quantity;
+
+    [SerializeField]
+    private Sprite sprite;
+
+    private InventoryManager inventoryManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit)) {
+                DetectItem(hit.collider);
+                Debug.Log("Clicked on: " + hit.collider.name);
+            }
+        }
+    }
+
+    public void DetectItem(Collider other) // pick up item after clicking on it
+    {
+            inventoryManager.AddItem(itemName, quantity, sprite);
+            Destroy(other.gameObject);
+    }
+}
