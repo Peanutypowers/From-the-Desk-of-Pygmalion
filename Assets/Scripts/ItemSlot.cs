65177d3e6819fd0efdf156f4b951f80e13d5fbf5
Peanutypowers <peanutypowers@gmail.com>
2026-06-21 14:53:45 -0400

    Completed item selection

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     // Item Data
     public string itemName;
@@ -17,6 +18,14 @@ public class ItemSlot : MonoBehaviour
     private TMP_Text quantityText;
     [SerializeField]
     private Image itemImage;
+    public GameObject selectedShader;
+    public bool thisItemSelected;
+    private InventoryManager inventoryManager;
+
+    private void Start()
+    {
+        inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
+    }
 
     public void AddItem(string itemName, int quantity, Sprite itemSprite)
     {
@@ -26,7 +35,29 @@ public class ItemSlot : MonoBehaviour
         isFull = true;
 
         quantityText.text = quantity.ToString();
-        quantityText.enabled = true;
         itemImage.sprite = itemSprite;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            OnLeftClick();
+        }        
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightClick();
+        }
+    }
+
+    public void OnLeftClick()
+    {
+        inventoryManager.DeselectAllSlots();
+        selectedShader.SetActive(true);
+        thisItemSelected = true;
+    }
+    public void OnRightClick()
+    {
+        
     }
 }
