76b3f447d6271df1060aa63c5011eaae462661c8
Peanutypowers <peanutypowers@gmail.com>
2026-07-20 15:37:03 -0400

    Created Additive Scene Loader

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+    }
+}
